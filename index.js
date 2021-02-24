aa653a78a636938f1b8c515285857dc6b765a735
soumya <79255255+soumyachikka@users.noreply.github.com>
2021-02-24 13:12:30 +0530

    update

--- index.js
+++ index.js
@@ -1,115 +1,83 @@
-// A constructor for defining new Rectangle
-function Rectangle(options) {
-  // some defaults
-  this.length = options.length || 4;
-  this.breadth = options.breadth || 4;
-  this.area = this.length * this.breadth;
+
+
+class shapeCalc {
+    constructor() {
+        this.selectShape = document.querySelector("#selectShape");
+        this.inputValueScreen = document.querySelector("#inputValues");
+        this.outputValues = document.querySelector("#outputValues");
+        this.areaValue = document.querySelector("#areaValue");
+        this.shapeType = document.querySelector("#shape");
+
+        this.width = document.querySelector("#width");
+        this.height = document.querySelector("#height");
+        this.radius = document.querySelector("#radius");
+        this.side = document.querySelector("#side");
+        this.length = document.querySelector("#length");
+        this.base = document.querySelector("#base");
+
+        this.area = 0;
+    }
+
+    static getShape() {
+        const elements = document.getElementsByName("shape");
+        for (let i=0; i<elements.length; i++) {
+            if (elements[i].checked) {
+                return elements[i].value;
+            }
+        }
+    }
+
+    inputValues () {
+        let shape = shapeCalc.getShape();
+        this.selectShape.style.display = 'none';
+        this.inputValueScreen.style.display = 'block';
+        this.shapeType.innerHTML = shape;
+
+        var elements = document.getElementById("showInputs").getElementsByTagName('input');
+        for (let i=0; i<elements.length; i++) {
+            elements[i].style.display = 'none';
+        }
+
+        if (shape === 'rectangle') {
+            this.length.style.display = 'block';
+            this.width.style.display = 'block';
+        } else if ( shape === 'circle') {
+            this.radius.style.display = 'block';
+        } else if (shape === 'square') {
+            this.side.style.display = 'block';
+        } else {
+            this.base.style.display = 'block';
+            this.height.style.display = 'block';
+        }
+    }
+
+    calculateArea() {
+        let shape = shapeCalc.getShape();
+        switch (shape) {            
+            case "rectangle":
+                this.area = this.length.value * this.width.value;
+            break;
+            case "circle":
+                this.area = Math.PI * this.radius.value * this.radius.value;
+                break;
+            case "square":
+                this.area = this.side.value * this.side.value;
+                break;
+            case "ellipse":
+                this.area = Math.PI * this.base.value * this.height.value;
+                break;            
+        }
+        this.area = Math.ceil(this.area);
+        this.showArea();
+    }
+
+    showArea() {
+        this.inputValueScreen.style.display = 'none';
+        this.outputValues.style.display = 'block';
+        this.areaValue.innerHTML = this.area;
+    }
 }
 
-// A constructor for defining new Circle
-function Circle(options) {
-  this.diameter = options.length || 4;
-  this.area = Math.PI * this.diameter * this.diameter;
-}
+let calc = new shapeCalc();
 
-// A constructor for defining new Sqaure
-function Sqaure(options) {
-  // some defaults
-  this.sides = options.length || 4;
-  this.area = this.sides * this.sides;
-}
 
-// A constructor for defining new Ellipse
-function Ellipse(options) {
-  this.majorAxis = options.length || 4;
-  this.minorAxis = options.breadth || 4;
-  this.area = Math.PI * this.majorAxis * this.minorAxis;
-}
-
-// Define a skeleton Shape factory
-function ShapeFactory() {}
-
-// Define the prototypes and utilities for this factory
-
-// Our default shapeClass is Rectangle
-ShapeFactory.prototype.shapeClass = Rectangle;
-
-ShapeFactory.prototype.createShape = function (options) {
-  switch (options.shapeType) {
-    case "rectangle":
-      this.shapeClass = Rectangle;
-      break;
-    case "circle":
-      this.shapeClass = Circle;
-      break;
-    case "square":
-      this.shapeClass = Sqaure;
-      break;
-    case "ellipse":
-      this.shapeClass = Ellipse;
-      break;
-  }
-
-  return new this.shapeClass(options);
-};
-
-function gotoStep2() {
-  // var mydata = JSON.parse(data);
-  this.checkedValue = document.querySelector(
-    'input[name="shape"]:checked'
-  ).value;
-  document.getElementById("title").innerHTML = "Step 2 : Insert your values";
-  document.getElementById("header").innerHTML =
-    "You have selected a " +
-    this.checkedValue +
-    ", please input the required value";
-  document.getElementById("selector").style.display = "none";
-  document.getElementById("step1").style.display = "none";
-  document.getElementById("step2").style.display = "block";
-  document.getElementById(this.checkedValue.toLowerCase()).style.display =
-    "block";
-}
-
-function gotoStep3() {
-  document.getElementById(this.checkedValue.toLowerCase()).style.display =
-    "none";
-  document.getElementById("title").innerHTML = "Step 3 : Your results";
-  document.getElementById("header").innerHTML =
-    "You have calculated the area of  " +
-    this.checkedValue +
-    ". Below is your result";
-  document.getElementById("step2").style.display = "none";
-  document.getElementById("step3").style.display = "block";
-  //let value1 = document.querySelector('[name="shapeValue"]').value;
-  var id = this.checkedValue.toLowerCase() + "length";
-  var id1 = this.checkedValue.toLowerCase() + "breadth";
-  let value1 = document.getElementById(id).value;
-  let value2 = "";
-  if (
-    this.checkedValue.toLowerCase() === "rectangle" ||
-    this.checkedValue.toLowerCase() === "ellipse"
-  ) {
-    value2 = document.getElementById(id1).value;
-  }
-
-  // Create an instance of our factory
-  var shape = new ShapeFactory();
-  this.areaofShape = shape.createShape({
-    shapeType: this.checkedValue.toLowerCase(),
-    length: value1,
-    breadth: value2 ? value2 : 0,
-  });
-  document.getElementById("area").style.display = "block";
-  document.getElementById("area").innerHTML =
-    "The Area is " + this.areaofShape.area;
-}
-
-function gotoStep1() {
-  document.getElementById("title").innerHTML = "Step 1 : Select your shape";
-  document.getElementById("area").style.display = "none";
-  document.getElementById("header").innerHTML =
-    "Please select the shape that would like to calculate the area of and press the button Go to step 2";
-  document.getElementById("step3").style.display = "none";
-  document.getElementById("selector").style.display = "block";
-  document.getElementById("step1").style.display = "block";
-}
